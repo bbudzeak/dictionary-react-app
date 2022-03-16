6acd7844a16f0a92616d88e47f4e1c08363ec9a6
bbudzeak <bzezulka@gmail.com>
2022-03-16 14:15:18 -0400

    Created dictionary app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <button type="button" class="btn btn-primary">
+            Primary
+          </button>
+        </header>
+        <h1>Hello World</h1>
+        <footer className="git-link">
+          <span> Project coded by Bethany Budzeak, </span>
+          <a
+            href="https://github.com/bbudzeak/weather-react-app-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
