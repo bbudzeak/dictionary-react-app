713e01d820943e52780cd362bd775b1b62a904cf
bbudzeak <bzezulka@gmail.com>
2022-03-18 17:17:05 -0400

    Added component to render search results

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -1,12 +1,16 @@
 import React, { useState } from "react";
 import "./SearchForm.css";
 import axios from "axios";
+import Results from "./Results";
 
 export default function SearchForm() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
     console.log(response.data[0]);
+    console.log(response.data[0].meanings[0].definitions[0].definition);
+    setResults(response.data[0]);
   }
 
   function search(event) {
@@ -24,6 +28,7 @@ export default function SearchForm() {
       <form onSubmit={search}>
         <input type="search" autoFocus={true} onChange={handleKeywordChange} />
       </form>
+      <Results results={results} />
     </div>
   );
 }
