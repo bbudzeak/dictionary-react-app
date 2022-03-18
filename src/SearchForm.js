0b1c98f84e8ee89fd46f703515fb3cd8beccdf0f
bbudzeak <bzezulka@gmail.com>
2022-03-18 13:43:04 -0400

    Added axios and api URL

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -1,12 +1,18 @@
 import React, { useState } from "react";
 import "./SearchForm.css";
+import axios from "axios";
 
 export default function SearchForm() {
   let [keyword, setKeyword] = useState("");
 
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
+
   function search(event) {
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     event.preventDefault();
-    alert(`Searching for ${keyword} definition`);
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
