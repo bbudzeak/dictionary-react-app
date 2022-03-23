b06eccb9880337355f9d648bd9db4da8b4854827
bbudzeak <bzezulka@gmail.com>
2022-03-23 15:36:52 -0400

    Added photo component

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -2,8 +2,6 @@ import React from "react";
 import "./Phonetic.css";
 
 export default function Phonetic(props) {
-  console.log(props);
-  console.log(props.phonetic.audio);
   return (
     <div className="Phonetic">
       <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
--- src/SearchForm.js
+++ src/SearchForm.js
@@ -2,20 +2,31 @@ import React, { useState } from "react";
 import "./SearchForm.css";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 
 export default function SearchForm() {
   let [keyword, setKeyword] = useState("");
   let [results, setResults] = useState(null);
+  let [photos, setPhotos] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data[0]);
     setResults(response.data[0]);
+  }
+
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
   }
 
   function search(event) {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     event.preventDefault();
     axios.get(apiUrl).then(handleResponse);
+
+    let pexelsApiKey =
+      "563492ad6f91700001000001342f30835f244f39a92d16f4e9fe589e";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=8`;
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
 
   function handleKeywordChange(event) {
@@ -30,12 +41,13 @@ export default function SearchForm() {
             type="search"
             autoFocus={true}
             onChange={handleKeywordChange}
-            defaultValue="Search word for definition"
+            defaultValue="Type word for definition"
           />
         </form>
         <div className="hint">suggested words: hike, travel, cook, coding</div>
       </section>
       <Results results={results} />
+      <Photos photos={photos} />
     </div>
   );
 }
